0d265fb0c55c74d7d6a625a5b0a618c95ee1a5a1
Ricky <rickyarddianto@outlook.com>
2020-06-23 19:33:56 +0700

    Props Pada ReactJs

--- aplikasipertama/src/Component/Fungsional/Home.jsx
+++ aplikasipertama/src/Component/Fungsional/Home.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Blog from './Blog'
 
 const Home = () => {
-    return <div><h1>Ini adalah Home</h1></div>
+    return <div>
+        <Blog
+            tanggal="21 Juni 2020"
+            judul="Teknologi Blockchain"
+            summary="Generate Lorem Ipsum placeholder text for use in your graphic."
+        />
+        <Blog
+            tanggal="22 Juni 2020"
+            judul="Teknologi Internet of Things"
+            summary="Generate Lorem Ipsum placeholder text for use in your graphic."
+        />
+        <Blog
+            tanggal="23 Juni 2020"
+            judul="Desain Patter Laravel"
+            summary="Generate Lorem Ipsum placeholder text for use in your graphic."
+        />
+
+    </div>
 }
 
 export default Home;
