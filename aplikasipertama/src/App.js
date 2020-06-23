3055b07c9357d18803d928513a7d80db769473c4
Ricky <rickyarddianto@outlook.com>
2020-06-23 23:00:49 +0700

    Passing Data Antar Komponet

--- aplikasipertama/src/App.js
+++ aplikasipertama/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Parent from './Component/Class/Parent';
+//import Home from './Comonent/Fungsional/Home';
+//import Beranda from './Comonent/Class/Beranda';
 //import './App.css';
-import Home from './Component/Fungsional/Home';
-import Beranda from './Component/Class/Beranda';
-
+ 
 function App() {
   return (
     <div className="App">
-      <Home/>
-    </div>
+     <Parent/>
+   </div>
   );
 }
 
