7a1f02dca7f45868d0c1f8477425352a22148249
Ricky <rickyarddianto@outlook.com>
2020-06-23 22:26:32 +0700

    State ReactJs

--- aplikasipertama/src/Component/Fungsional/Home.jsx
+++ aplikasipertama/src/Component/Fungsional/Home.jsx
@@ -1,24 +1,15 @@
 import React from 'react'
 import Blog from './Blog'
+import Produk from '../Class/Produk';
 
 const Home = () => {
     return <div>
-        <Blog
-            tanggal="21 Juni 2020"
-            judul="Teknologi Blockchain"
-            summary="Generate Lorem Ipsum placeholder text for use in your graphic."
-        />
-        <Blog
-            tanggal="22 Juni 2020"
-            judul="Teknologi Internet of Things"
-            summary="Generate Lorem Ipsum placeholder text for use in your graphic."
-        />
-        <Blog
-            tanggal="23 Juni 2020"
-            judul="Desain Patter Laravel"
-            summary="Generate Lorem Ipsum placeholder text for use in your graphic."
-        />
-
+        <Produk nama="Macbook Pro 2020" stock="10" harga="33000000" />
+        <Produk nama="Macbook Pro 2018" stock="15" harga="43000000" />
+        <Produk nama="Macbook Pro 2021" stock="30" harga="53000000" />
+        <Produk nama="Macbook Pro 2022" stock="25" harga="23000000" />
+        <Produk nama="Macbook Pro 2019" stock="50" harga="63000000" />
+        <Produk nama="Macbook Pro 2023" stock="23" harga="73000000" />
     </div>
 }
 
