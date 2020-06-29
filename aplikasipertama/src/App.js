aa75f4ba3b0cef8e88f8c0ec2b75fcdcfd20ab56
Ricky <rickyarddianto@outlook.com>
2020-06-29 14:49:12 +0700

    Cara menggunakan React Router

--- aplikasipertama/src/App.js
+++ aplikasipertama/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import BoostrapComp from './Component/Class/BootstrapComp';
+//import BoostrapComp from './Component/Class/BootstrapComp';
+import NavbarComp from './Component/Fungsional/NavbarComp';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+//import BoostrapComp from './Comonent/Class/BootstrapComp';
+import HomePage from './Component/Fungsional/HomePage';
+import About from './Component/Fungsional/AboutComp';
+import { Form } from 'reactstrap';
 //import Home from './Comonent/Fungsional/Home';
 //import Beranda from './Comonent/Class/Beranda';
 //import './App.css';
  
-function App() {
+const App = () => {
   return (
-    <div className="App">
-     <BoostrapComp/>
-   </div>
+    <BrowserRouter>
+    <NavbarComp />
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/about" component={About} />
+    </Switch>
+
+  </BrowserRouter>
   );
 }
 
