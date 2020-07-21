83d85fe452764fec6060755854623f73a93ad259
Ricky <rickyarddianto@outlook.com>
2020-07-21 10:43:51 +0700

    React Hooks useEffects

--- aplikasipertama/src/App.js
+++ aplikasipertama/src/App.js
@@ -13,6 +13,8 @@ import TambahComp from './Component/Class/Tambah';
 import EditComp from './Component/Class/EditComp';
 import KelasComp from './Component/Hooks/Class/KelasComp';
 import HooksComp from './Component/Hooks/Functional/HooksComp';
+import HooksUseEffects from './Component/Hooks/Functional/HooksUseEffects';
+
 // import BootstrapComp from './Component/Class/BootstrapComp';
 //import Parent from './Component/Class/Parent';
 // import logo from './logo.svg';
@@ -33,6 +35,7 @@ const App = () => {
       <Route exact path="/mahasiswa/edit" component={EditComp} />
       <Route exact path="/kelas" component={KelasComp} />
         <Route exact path="/hooks" component={HooksComp} />
+        <Route exact path="/useeffects" component={HooksUseEffects} />
     
        {/* <Route exact path="/detail/:id" component={DetailComp} /> */}
     </Switch>
