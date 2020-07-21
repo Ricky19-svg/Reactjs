7521103f3d350542d9699cfc9a7d76005bc8580b
Ricky <rickyarddianto@outlook.com>
2020-07-21 13:04:32 +0700

    React Hooks useReducer + Context

--- aplikasipertama/src/App.js
+++ aplikasipertama/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, createContext, useReducer} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 //import BoostrapComp from './Component/Class/BootstrapComp';
 import NavbarComp from './Component/Fungsional/NavbarComp';
@@ -16,6 +16,7 @@ import HooksUseEffects from './Component/Hooks/Functional/HooksUseEffects';
 import { CartContext } from './CartContext';
 import ProductComp from './Component/Hooks/Functional/ProductComp';
 import HooksReducer from './Component/Hooks/Functional/HooksReducer';
+import Tagihan from './Component/Hooks/Functional/Tagihan';
 
 // import BootstrapComp from './Component/Class/BootstrapComp';
 //import Parent from './Component/Class/Parent';
@@ -24,14 +25,41 @@ import HooksReducer from './Component/Hooks/Functional/HooksReducer';
 //import Beranda from './Component/Class/Beranda';
 //import './App.css';
  
+const initialState = {
+  jumlah: 1,
+  hargasatuan: 10000,
+  hargatotal: 10000
+}
+
+export const keranjangContext = createContext()
+
+const reducer = (state, action) => {
+  switch (action. type) {
+      case 'tambah': return {
+          ...state,
+          jumlah: state.jumlah + 1,
+          hargatotal: state.hargasatuan + (state.hargasatuan * state.jumlah)
+      }
+      case 'kurang': return {
+          ...state,
+          jumlah: state.jumlah - 1,
+          hargatotal: (state.hargasatuan * state.jumlah) - state.hargasatuan
+      }
+      default:
+          return state
+  }
+}
+
 const App = () => {
 
   const [value, setValue] = useState(0)
+  const[count, dispatch] = useReducer(reducer, initialState)
 
   return (
     <BrowserRouter>
     <CartContext.Provider value={{value, setValue}}>
         <NavbarComp />
+        <keranjangContext.Provider value={{keranjangState: count, keranjangDispatch:dispatch}}>
         <switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/about" component={About} />
@@ -43,9 +71,11 @@ const App = () => {
           <Route exact path="/useeffects" component={HooksUseEffects} />
           <Route exact path="/produk" component={ProductComp} />
           <Route exact path="/reducer" component={HooksReducer} />
+          <Route exact path="/tagihan" component={Tagihan} />
 
           {/*<Route exact path="/detail/:id" component={DetailComp} />*/}
         </switch>
+        </keranjangContext.Provider>
       </CartContext.Provider>
   </BrowserRouter>
   );
